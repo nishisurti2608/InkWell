4070543f07a2ec21808ca604313acacce84cf738
joan <nutsukpuijoan@gmail.com>
2025-07-22 20:24:35 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className=" text-3xl">Begin of the project the project  i already add taiwindcss into it</h1>
+    </>
+  )
+}
+
+export default App
